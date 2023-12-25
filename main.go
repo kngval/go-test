fdc727581e9411b5af21baf99b7f0ce4e0139bea
kngval <bkvalteocs@gmail.com>
2023-12-25 20:52:30 +0800

    data types

---
+++ main.go
@@ -0,0 +1,15 @@
+
+package main
+
+import "fmt"
+
+func main(){
+	
+	//Varibales
+	// 1. Var
+	var newLanguage string = "Hello World"; 
+	var Greetings  = "Merry Christmas";
+	fmt.Println(newLanguage,Greetings)
+
+
+}
